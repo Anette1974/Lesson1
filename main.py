7a8227960b2455639e668d388b85cadd30e0aa69
Anette1974 <anette.cedergren@yahoo.se>
2025-01-10 13:31:23 +0100

    Övning 1 & 2

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
+# Veckouppgift 1
 # Övning 1
 #https://github.com/Anette1974/Lesson1.git
+print ("ÖVNING 1")
 first_name = "Anette"
 last_name = "Cedergren"
 print ("Hello world")
-print ("This program was made by " +first_name +" "+last_name)
+print ("This program was made by " +first_name +" "+last_name +"\n")
+
+# Övning 2
+'''
+2 Diskutera i grupp
+Rätta eventuella fel. Vad gör koden?
+x = 100  # biljettpris
+y = 200  # pengar på fickan
+print("Det blir " + (y - x) " kronor över.")
+z = 200 - 100 / 2
+print("Hälften är: " + z)
+Kom på bättre namn, i stället för x, y och z.
+'''
+print ("ÖVNING 2")
+biljettpris = 100  # biljettpris
+kontanter = 200  # pengar på fickan
+print("Det blir " + str(kontanter - biljettpris) + " kronor över.")  # Konvertera resultatet till sträng
+z = 200 - 100 / 2
+print("Hälften är: " + str(z))  # Konvertera z till sträng
+
+# Övning 3
+'''
+3 Använda variabler och datatyper
+1a Använd input för att be användaren om ett heltal. Spara värdet i en variabel. Omvandla variabelns värde till ett heltal, och skriv ut det för att testa om du har gjort rätt.
+Kodexempel med input:
+x = input("Fråga här")
+
+1b Fråga användaren efter ett annat heltal. Skriv ut summan av talen, alltså tal1 + tal2.
+Testa genom att hitta på två tal och räkna ut summan i huvudet. Kontrollera om programmet räknar rätt.
+
+2a Nu är det dags att köpa vinterkläder. Du ser en fin jacka som kostar 2000 kronor. Jackan är på rea, 50%. Skriv ett program som räknar ut hur mycket du behöver betala.
+Tips: räkna ut rabatten med formeln: slut_pris = pris * rea_procent / 100.
+
+2b Gör om programmet så att användaren kan skriva in en procentsats.
+Testa genom att hitta på en procentsats och räkna ut vad programmet ska svara med, innan du kör det. Till exempel 10%, som är 200 kr. Då ska jackan kosta 2000 - 200 == 1800 kr.
+'''
